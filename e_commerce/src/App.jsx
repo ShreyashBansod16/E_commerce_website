29ca5b2b3c29ae3f1e5c85fa900ddfe03fe1046a
ShreyashBansod16 <shreyashbansod72@gmail.com>
2024-09-12 17:33:37 +0530

    first commit

---
+++ e_commerce/src/App.jsx
@@ -0,0 +1,17 @@
+import tailwindConfig from "../tailwind.config";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+const App = () => {
+  return (
+    <main className="overflow-x-hidden">
+    <Navbar></Navbar>
+    <Hero>
+      
+    </Hero>
+   
+
+    </main>
+  
+  )}
+
+export default App;
---
+++ e_commerce/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { FaLeaf } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+import { MdMenu } from "react-icons/md";
+import RespnsiveMenu from './RespnsiveMenu';
+import { FaStore } from "react-icons/fa";
+
+
+const NavbarMenu=[
+    {
+        id:1,
+        title:"Home",
+        link:"/"
+    },
+    {
+        id:2,
+        title:"Products",
+        link:"/"
+    },
+    {
+        id:3,
+        title:"About",
+        link:"/"
+    },
+    {
+        id:4,
+        title:"Shop",
+        link:"/"
+    },
+    {
+        id:5,
+        title:"Contacts",
+        link:"/"
+    }, 
+];
+
+
+const Navbar = () => {
+  const [open,setopen]=useState(false);
+  
+  
+    return (
+        <>
+   <nav className='md:pt-4 relative z-30 bg-white'>
+    <div className="container  py-4 flex  justify-between  items-center  md:pt-4">
+        {/*logo selection*/}
+        <div className='text-2xl flex items-center font-bold gap-4 uppercase '>
+            <p className='text-primary'>E-commerce</p>
+            <p className="text-secondary">  Store</p>
+            <FaStore className='text-green-400'/>
+        </div>
+        {/*Menu selection*/}
+        <div className='hidden md:block '>
+            <ul className='flex items-center gap-6 text-gray-600'>
+                {NavbarMenu.map((menu) => (
+                    <li key={menu.id} >
+                <a href={menu.link}
+                className="inline-block py-1 px-3 hover:text-primary hover:shadow-[0_3px_0_-1px_#ef4444] font-semibold ">{menu.title}</a>
+                </li>
+                ))}
+                <button className='text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200' >
+                    <MdOutlineShoppingCart></MdOutlineShoppingCart>
+                </button>
+                
+            </ul>
+        </div>
+        {/*mobile*/}
+        <div className='md:hidden' onClick={()=>
+            setopen(!open)
+        }>
+            <MdMenu className='text-4xl'/>
+
+        </div>
+        </div>
+   </nav>
+    {/*Mobiile menu */}
+   < RespnsiveMenu open={open} />
+</>
+  )
+}
+
+export default Navbar;
